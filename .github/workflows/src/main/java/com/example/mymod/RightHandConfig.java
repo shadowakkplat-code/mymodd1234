b6bdcd473121f0b1b3b541b6cbe3c132b1bf89d7
shadowakkplat-code <shadowakkplat@gmail.com>
2026-05-26 14:13:36 +0300

    Create RightHandConfig.java

---
+++ .github/workflows/src/main/java/com/example/mymod/RightHandConfig.java
@@ -0,0 +1,17 @@
+package com.example.mymod;
+
+public class RightHandConfig {
+    public static float rightX = 0.12f; 
+    public static float rightY = 0.10f;
+    public static float rightZ = -0.45f;
+
+    public static float leftX = -0.315f; 
+    public static float leftY = 0.10f;
+    public static float leftZ = -0.45f;
+    
+    public static int leftScalePercent = 50;
+    public static int rightScalePercent = 0;
+    
+    public static int swingMode = 0; 
+    public static int activeParticleId = 0;
+}
